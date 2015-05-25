3325abf2fee8ba67aea1e3e6e5310c258719955a
Moneil <cam8059@yahoo.com>
2015-05-25 17:52:58 -0500

    Fixed Issue with going across corners

--- src/PathFindingCenterPanel.java
+++ src/PathFindingCenterPanel.java
@@ -1,7 +1,6 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -21,23 +20,20 @@ public class PathFindingCenterPanel extends JPanel{
 			@Override
 			public void run() {
 
-				//List<Cell> open = new ArrayList<Cell>();
-				//List<Cell> closed = new ArrayList<Cell>();
 				endCell = getEndCell();
 				Cell start = getStartCell();
 				start.gCost = 0;
 				start.hCost = getCostBetween(start, endCell);
 				open.add(start);
+				
+				say(getAdjacent(start));
+				
 				if (open.get(0) == null){
 					System.err.println("no start");
 					return;
 				}
 				say(open);
 
-				
-				
-				//start loop
-				//for (int i=0; i < 4; i++){
 				while (true){
 				
 					Cell current = getLowestOpenFCost();
@@ -49,8 +45,6 @@ public class PathFindingCenterPanel extends JPanel{
 						say("we done");
 						say(closed);
 						repaint();
-						
-						//while (cell.)
 						
 						Cell cell = endCell.parentCell;
 						cell.setStatus(CellStatus.FINAL);
@@ -71,6 +65,9 @@ public class PathFindingCenterPanel extends JPanel{
 						for (int c=Math.max(0, current.getCol()-1); c <= Math.min(Runner.cells[r].length-1 ,current.getCol()+1); c++){
 							
 							Cell neighbor = Runner.cells[r][c];
+							
+							if (checkIfCorner(current, neighbor))
+								continue;
 							
 							if ((neighbor.getType() == CellTypes.EMPTY || neighbor.getType() == CellTypes.END) && !closed.contains(neighbor)){
 								
@@ -102,44 +99,7 @@ public class PathFindingCenterPanel extends JPanel{
 						}
 					}
 				
-				
-				}//End loop
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				/*print2D(Runner.cells);
-				updateCostsAround(open.get(0));
-				print2D(Runner.cells);
-				
-				say("");
-				say("lowest: " + getLowestFCost());
-				
-				repaint();
-				
-				closed.add(start);
-				
-				say("lowest: " + getLowestFCost());*/
-				
-				
-				
-//				while (true){
-//					
-//					
-//
-//					try {
-//						Thread.sleep(Runner.currentDelay);
-//					} catch (InterruptedException e) {
-//						e.printStackTrace();
-//					}
-//				}
-				
+				}
 				
 			}
 			
@@ -147,29 +107,79 @@ public class PathFindingCenterPanel extends JPanel{
 			//hCost -dist to end //should stay same
 			//fCost -total
 
-			private void updateCostsAround(Cell currentCell) {
+			private boolean checkIfCorner(Cell current, Cell neighbor) {
 				
-				for (int r=Math.max(0, currentCell.getRow()-1); r <= Math.min(Runner.cells.length, currentCell.getRow()+1); r++)
-					for (int c=Math.max(0, currentCell.getCol()-1); c <= Math.min(Runner.cells[r].length ,currentCell.getCol()+1); c++){
-						
-						if (!closed.contains(Runner.cells[r][c])){
-						
-							//say("r: " + r + " c: " + c);
-							//TODO Need to fix this later, needs to check distance from parent cell and add it to parent's gCost //Done?
-							double newGCost = currentCell.gCost + getCostBetween(currentCell, Runner.cells[r][c]);
-							if (Runner.cells[r][c].gCost > newGCost){
-								Runner.cells[r][c].gCost = newGCost;
-								Runner.cells[r][c].parentCell = currentCell;
-							}
-							
-							Runner.cells[r][c].hCost = getCostBetween(Runner.cells[r][c], endCell);
-							
-							Runner.cells[r][c].fCost = Runner.cells[r][c].hCost + Runner.cells[r][c].gCost;
+				List<Cell> currentBarriers = new ArrayList<Cell>();
+				
+				for (Cell cell : getAdjacent(current)){
+					if (cell.getType() == CellTypes.BARRIER)
+						currentBarriers.add(cell);
+				}
+				
+				if (currentBarriers.size() < 2)
+					return false;
+				
+				List<Cell> neighborBarriers = new ArrayList<Cell>();
+				
+				for (Cell cell : getAdjacent(neighbor)){
+					if (cell.getType() == CellTypes.BARRIER)
+						neighborBarriers.add(cell);
+				}
+				
+				int similar = 0;
+				
+				for (Cell a : currentBarriers)
+					for (Cell b : neighborBarriers)
+						if (a == b){
+							similar++;
+							break;
 						}
-					}
-						
+				
+				return similar >=2;
+				
 				
 			}
+			
+			private List<Cell> getAdjacent(Cell cell){
+				List<Cell> cells = new ArrayList<Cell>();
+				int row = cell.getRow(), col = cell.getCol();
+				
+				int r[] = {row,row,row+1,row-1};
+				int c[] = {col+1,col-1,col,col};
+					
+					for (int i=0; i < 4; i++)
+						if (r[i] >= 0 && r[i] < Runner.cells.length && c[i] >= 0 && c[i] < Runner.cells[r[i]].length)
+							cells.add(Runner.cells[r[i]][c[i]]);
+				
+				if (cells.size() > 4)
+					System.err.println(" > 4");
+				
+				return cells;
+			}
+
+//			private void updateCostsAround(Cell currentCell) {
+//				
+//				for (int r=Math.max(0, currentCell.getRow()-1); r <= Math.min(Runner.cells.length, currentCell.getRow()+1); r++)
+//					for (int c=Math.max(0, currentCell.getCol()-1); c <= Math.min(Runner.cells[r].length ,currentCell.getCol()+1); c++){
+//						
+//						if (!closed.contains(Runner.cells[r][c])){
+//						
+//							//say("r: " + r + " c: " + c);
+//							//TODO Need to fix this later, needs to check distance from parent cell and add it to parent's gCost //Done?
+//							double newGCost = currentCell.gCost + getCostBetween(currentCell, Runner.cells[r][c]);
+//							if (Runner.cells[r][c].gCost > newGCost){
+//								Runner.cells[r][c].gCost = newGCost;
+//								Runner.cells[r][c].parentCell = currentCell;
+//							}
+//							
+//							Runner.cells[r][c].hCost = getCostBetween(Runner.cells[r][c], endCell);
+//							
+//							Runner.cells[r][c].fCost = Runner.cells[r][c].hCost + Runner.cells[r][c].gCost;
+//						}
+//					}
+//						
+//				
+//			}
 			
 			private double getCostBetween(Cell a, Cell b){
 				int height = Math.abs(a.getRow() - b.getRow());
@@ -190,16 +200,6 @@ public class PathFindingCenterPanel extends JPanel{
 						lowest = cell;
 				
 				return lowest;
-					
-				
-//				Cell lowest = Runner.cells[0][0];
-//				
-//				for (Cell[] row : Runner.cells)
-//					for (Cell cell : row)
-//						if (lowest.fCost > cell.fCost)
-//							lowest = cell;
-//				
-//				return lowest;
 			}
 			
 			private Cell getStartCell(){
@@ -223,10 +223,10 @@ public class PathFindingCenterPanel extends JPanel{
 		
 	}
 	
-	private void print2D(Object[][] array){
-		for (Object[] row : array)
-		    System.out.println(Arrays.toString(row));
-	}
+//	private void print2D(Object[][] array){
+//		for (Object[] row : array)
+//		    System.out.println(Arrays.toString(row));
+//	}
 	
 	@Override
 	protected void paintComponent(Graphics g1) {
