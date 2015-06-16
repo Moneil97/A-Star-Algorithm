a64698ca1755d63cbd07d8c60b542894895b55cf
Moneil <cam8059@yahoo.com>
2015-06-16 00:27:51 -0500

    Forces only one start and end point

--- src/EditorCenterPanel.java
+++ src/EditorCenterPanel.java
@@ -36,12 +36,26 @@ public class EditorCenterPanel extends JPanel{
 		
 	}
 	
+	Cell currentStart, currentEnd;
+	
 	private void press(MouseEvent e){
+		
+		if (Runner.currentCellTypeSelection == CellTypes.START && currentStart != null)
+				currentStart.setType(CellTypes.EMPTY);
+		else if (Runner.currentCellTypeSelection == CellTypes.END && currentEnd != null)
+			currentEnd.setType(CellTypes.EMPTY);
 		
 		for (Cell[] row : Runner.cells)
 			for (Cell cell : row)
-				if (cell.contains(e))
+				if (cell.contains(e)){
+					
 					cell.setType(Runner.currentCellTypeSelection);
+					
+					if (Runner.currentCellTypeSelection == CellTypes.START)
+						currentStart = cell;
+					else if (Runner.currentCellTypeSelection == CellTypes.END)
+						currentEnd = cell;
+				}
 		
 		repaint();
 		
